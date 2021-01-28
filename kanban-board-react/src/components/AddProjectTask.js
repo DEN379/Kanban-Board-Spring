936d2a131b28315ea0d63d39fdd91ec636a82f7f
Dennis Sakadel <denyssakadel@gmail.com>
2021-01-28 21:44:44 +0200

    added error action in form

--- kanban-board-react/src/components/AddProjectTask.js
+++ kanban-board-react/src/components/AddProjectTask.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {addProjectTask} from "../actions/projectTaskActions";
+import classnames from "classnames";
 
 class AddProjectTask extends Component {
     constructor(){
         super();
         this.state = {
-            "summary": "",
-            "acceptanceCriteria": "",
-            "status": ""
+            summary: "",
+            acceptanceCriteria: "",
+            status: "",
+            errors: {}
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            this.setState({errors: nextProps.errors})
+        }
+    }
+
     onChange(e){
         this.setState({[e.target.name]:e.target.value})
     }
     onSubmit(e){
         e.preventDefault();
-        const projectTask = {
-            "summary": this.state.summary,
-            "acceptanceCriteria": this.state.acceptanceCriteria,
-            "status": this.state.status
+        const newProjectTask = {
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status
         }
-        console.log(projectTask);
+        //console.log(newProjectTask);
+        this.props.addProjectTask(newProjectTask, this.props.history);
     }
     render() {
+        const {errors} = this.state;
         return (
             <div className="addProjectTask">
         <div className="container">
@@ -36,8 +49,13 @@ class AddProjectTask extends Component {
                     <h4 className="display-4 text-center">Add /Update Project Task</h4>
                     <form onSubmit={this.onSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" name="summary" 
+                            <input type="text" className={classnames("form-control form-control-lg", 
+                                {"is-invalid": errors.summary})}
+                             name="summary" 
                             value={this.state.summary} onChange={this.onChange} placeholder="Project Task summary" />
+                            {errors.summary && (
+                            <div className="invalid-feedback">{errors.summary}</div>
+                        )}
                         </div>
                         <div className="form-group">
                             <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" 
@@ -62,4 +80,14 @@ class AddProjectTask extends Component {
         )
     }
 }
-export default AddProjectTask;
+
+AddProjectTask.propTypes = {
+    addProjectTask: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateProps = state =>({
+    errors: state.errors
+});
+
+export default connect(mapStateProps, {addProjectTask}) (AddProjectTask);
