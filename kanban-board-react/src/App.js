761cd78341762b5ebb093d4161ce0065cf3e1bad
Dennis Sakadel <denyssakadel@gmail.com>
2021-01-28 19:13:20 +0200

    added provider and reducers

--- kanban-board-react/src/App.js
+++ kanban-board-react/src/App.js
@@ -4,16 +4,21 @@ import NavBar from './components/NavBar';
 import KanbanBoard from './components/KanbanBoard';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AddProjectTask from './components/AddProjectTask';
+import {Provider} from 'react-redux';
+import store from "./store";
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar></NavBar>
-        <Route exact path="/" component={KanbanBoard}></Route>
-        <Route exact path="/addProjectTask" component={AddProjectTask}></Route>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <NavBar></NavBar>
+          <Route exact path="/" component={KanbanBoard}></Route>
+          <Route exact path="/addProjectTask" component={AddProjectTask}></Route>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
