03b32cf615dbb982de0c6f0d5dbcb7c3ea4bcf7f
Dennis Sakadel <denyssakadel@gmail.com>
2021-02-01 14:47:40 +0200

    added update function and fix reference

--- kanban-board-react/src/App.js
+++ kanban-board-react/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AddProjectTask from './components/AddProjectTask';
 import {Provider} from 'react-redux';
 import store from "./store";
+import UpdateProjectTask from './components/UpdateProjectTask';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <NavBar></NavBar>
           <Route exact path="/" component={KanbanBoard}></Route>
           <Route exact path="/addProjectTask" component={AddProjectTask}></Route>
+          <Route exact path="/updateProjectTaks/:id" component={UpdateProjectTask}></Route>
         </div>
       </Router>
     </Provider>
--- kanban-board-react/src/actions/projectTaskActions.js
+++ kanban-board-react/src/actions/projectTaskActions.js
@@ -1,21 +1,21 @@
 import axios from "axios";
-import { DELETE_PROJECT_TASK, GET_ERRORS, GET_PROJECT_TASKS } from "./types";
+import { DELETE_PROJECT_TASK, GET_ERRORS, GET_PROJECT_TASKS, GET_PROJECT_TASK } from "./types";
 
-export const addProjectTask = (project_task, history) => async dispatch =>{
-    try{
-        await axios.post("http://localhost:8080/api/board", project_task);
-        history.push("/");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch(error){
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
+export const addProjectTask = (project_task, history) => async dispatch => {
+    try {
+      await axios.post("http://localhost:8080/api/board", project_task);
+      history.push("/");
+      dispatch({
+        type: GET_ERRORS,
+        payload: {}
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      });
     }
-};
+  };
 
 export const getBacklog = () => async dispatch => {
     const res = await axios.get("http://localhost:8080/api/board/all");
@@ -34,4 +34,17 @@ export const deleteProjectTask = id => async dispatch =>{
         });
     }
     
+};
+
+
+export const getProjectTask = (id, history) => async dispatch => {
+    try{
+        const res = await axios.get(`http://localhost:8080/api/board/${id}`);
+        dispatch({
+            type: GET_PROJECT_TASK,
+            payload: res.data
+        });
+    } catch(error){
+        history.push("/");
+    }
 };
